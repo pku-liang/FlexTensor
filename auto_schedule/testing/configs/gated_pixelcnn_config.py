bc0538564fc94fc542493561f7cd89f335c5f4d4
holmosaint <1600012947@pku.edu.cn>
2019-07-23 20:35:50 +0800

    Add autate gated pixel cnn

--- auto_schedule/testing/configs/gated_pixelcnn_config.py
+++ auto_schedule/testing/configs/gated_pixelcnn_config.py
@@ -1,5 +1,5 @@
 """
-    batch_size, input_height, input_width, in_channels, out_channels, kernelheight, kernel_width, ClassVector=None, bias=None, dilation=1, stride=1, padding=0
+    batch_size, input_height, input_width, in_channels, out_channels, kernel_height, kernel_width, ClassVector=None, bias=None, dilation=1, stride=1, padding=0
 """
 gated_pixelcnn_shape = [
     (1, 256, 256, 3, 256, 3, None, None, 1, 1, 0)
