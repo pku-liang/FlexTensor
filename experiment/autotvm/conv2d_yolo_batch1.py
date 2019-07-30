c90416b229453aa3a18c24ba2bd1a078da7388c5
crz <crz@pku.edu.cn>
2019-07-30 16:16:54 +0800

    !!!

---
+++ experiment/autotvm/conv2d_yolo_batch1.py
@@ -0,0 +1,15 @@
+from experiment.util.autotvm_schedule import conv2d_schedule_dispatcher, conv2d_evaluate
+from experiment.util.autotvm_test import main
+from experiment.shape import conv2d_batch1_shapes as shapes
+import tvm
+from tvm import autotvm
+import topi
+
+if __name__ == '__main__':
+    name = "conv2d_yolo_batch32"
+
+    def trans(shape):
+        _, C, H, W, K, _, Hk, _, _, stride, padding, dilation, _ = shape
+        return (1, C, H, W, K, Hk, stride, padding, dilation, 1)
+
+    main(name, conv2d_schedule_dispatcher, map(trans, shapes), conv2d_evaluate)
---
+++ experiment/optimize/conv2d_yolo_batch1.py
@@ -0,0 +1,8 @@
+from experiment.util.optimize_test import main
+from experiment.shape import conv2d_batch1_shapes as shapes
+
+if __name__ == '__main__':
+    def trans(shape):
+        _, C, H, W, K, _, Hk, _, _, stride, padding, dilation, groups = shape
+        return (1, C, H, W, K, Hk, stride, padding, dilation, groups)
+    main('conv2d', __file__[:-3], shapes, trans)
