8ddc5a4db88eab0dec6a0ffbbdabe2db5cf9b40d
Anjiang Wei <davidwei@scc.pkusc.org>
2020-04-20 10:06:15 +0800

    add downcast test using numpy

--- flextensor/test/test_tvm_expr/grad/te-downcast-case1.py
+++ flextensor/test/test_tvm_expr/grad/te-downcast-case1.py
@@ -1,4 +1,5 @@
 import tvm
+import numpy as np
 
 
 H = 8
@@ -10,10 +11,31 @@ C = tvm.te.compute([H],
   lambda i:
     A[i, i], name="C")
 
-dC = tvm.te.compute([H], lambda h: 1.0, name="dC")
+dC = tvm.te.placeholder([H], dtype=dtype, name="dC")
 
 dA = tvm.te.grad_op(A, C, dC)
 
 s = tvm.te.create_schedule(dA.op)
 
 print(tvm.lower(s, [A, dC, dA], simple_mode=True))
+
+func = tvm.build(s, [A, dC, dA], target="llvm")
+
+A_np = np.random.uniform(-10, 10, [H, H]).astype("float32")
+dC_np = np.random.uniform(-10, 10, [H]).astype("float32")
+dA_np = np.zeros([H, H]).astype("float32")
+
+ctx = tvm.context("llvm", 0)
+A_tvm = tvm.nd.array(A_np, ctx)
+dC_tvm = tvm.nd.array(dC_np, ctx)
+dA_tvm = tvm.nd.array(dA_np, ctx)
+
+func(A_tvm, dC_tvm, dA_tvm)
+
+print(dA_tvm)
+
+# =======>
+# compare the results with numpy
+golden_np = np.diag(dC_np)
+tvm.testing.assert_allclose(dA_tvm.asnumpy(), golden_np, rtol=1e-30)
+print("Compare with Numpy success!")
