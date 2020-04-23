37c5b621871c85f2c5946d66b44cf9ccb79c4e0e
Anjiang Wei <davidwei@scc.pkusc.org>
2020-04-23 15:10:58 +0800

    add tanh test success

--- flextensor/test/test_tvm_expr/grad/tir-tanh-case1.py
+++ flextensor/test/test_tvm_expr/grad/tir-tanh-case1.py
@@ -5,8 +5,6 @@ import torch
 
 dim1 = 8
 dim2 = 4
-# dim3 = 3
-# dim4 = 5
 shape_size = [dim1, dim2]
 dtype = "float32"
 
@@ -38,12 +36,13 @@ func(A_tvm, dC_tvm, dA_tvm)
 print("dA_tvm", dA_tvm)
 
 # =======>
-# compare the results with numpy
-print("dA_np", A_np)
-golden_np = np.ones(shape_size) - np.power(A_np, 2)
-print("dC_np", dC_np)
-print("golden_np", golden_np)
-tvm.testing.assert_allclose(dA_tvm.asnumpy(), golden_np, rtol=1e-5)
-print("Compare with Numpy success!")
+# compare the results with pytorch
+A_torch = torch.tensor(A_np, requires_grad=True)
+C_torch = torch.tanh(A_torch)
+loss = C_torch.sum()
+loss.backward()
+print("Pytorch gradient:\n", A_torch.grad.numpy())
+tvm.testing.assert_allclose(dA_tvm.asnumpy(), A_torch.grad.numpy(), atol=1e-6, rtol=1e-7)
+print("Compare with PyTorch success!")
 
 
